59a343ecd9e007e1b851713168d3f55d8b474102
cantrkdgn <cantrkdgn@gmail.com>
2021-09-01 23:16:02 +0300

    first commit

---
+++ js/odev.js
@@ -0,0 +1,34 @@
+// **** Javascript İsim ve Saat Yerlestirme Odevi
+
+let userName = prompt("Lütfen İsminizi Giriniz");
+let info = document.querySelector('#info')
+info.innerHTML += userName
+
+
+// let time;
+// setInterval(() => {
+//     a = new Date();
+//     time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+//     document.querySelector('#span').innerHTML = time;
+//   }, 1000);
+
+
+
+// span.innerHTML = time
+
+const days = ['Pazar', 'Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi'];
+
+
+setInterval(showTime,1000);
+function showTime(){
+    let time = new Date;
+    let hour = time.getHours();
+    let min = time.getMinutes();      
+    let sec = time.getSeconds();
+    let day = days[time.getDay()];
+
+    let currentTime = hour + ":" + min + ":" + sec +" "+ day;
+
+    document.getElementById("span").innerHTML = currentTime;
+    
+}
